14aabf0906705fb0703e0ce6fd894632ae39c09b
humna-dev <hamnaallauddin4@gmail.com>
2024-06-06 07:03:08 -0700

    Done

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
-#!/usr/bin/env node
-import inquirer from "inquirer";
+#! /usr/bin/env node
+import inquirer from 'inquirer';
 import chalk from "chalk";
-const answer = await inquirer.prompt([
-    { message: chalk.green("Enter first number"), type: "number", name: "firstNumber" },
-    { message: chalk.green("Enter second number"), type: "number", name: "secondNumber" },
+let myBalance = 50000; //Dollars
+let myPin = 6767;
+console.log(chalk.yellowBright("Welcome to my ATM Machine!"));
+let pinAnswer = await inquirer.prompt([
     {
-        message: chalk.bgRed("Select one of the operators to perform operation"),
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
+        name: "pin",
+        message: chalk.red("Enter your Pin"),
+        type: "number"
+    }
 ]);
-if (answer.operator === "Addition") {
-    console.log(chalk.yellow(answer.firstNumber + answer.secondNumber));
-}
-else if (answer.operator === "Subtraction") {
-    console.log(chalk.yellow(answer.firstNumber - answer.secondNumber));
-}
-else if (answer.operator === "Multiplication") {
-    console.log(chalk.yellow(answer.firstNumber * answer.secondNumber));
-}
-else if (answer.operator === "Division") {
-    console.log(chalk.yellow(answer.firstNumber / answer.secondNumber));
+if (pinAnswer.pin === myPin) {
+    console.log(chalk.bgBlueBright("Correct pin Code!!!"));
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: chalk.greenBright("Please Select Option"),
+            type: "list",
+            choices: ["Withdraw", "Check Balance", "Fast Cash", "Exit"]
+        }
+    ]);
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: chalk.cyan("Enter the amount to withdraw"),
+                type: "number"
+            }
+        ]);
+        if (amountAns.amount > myBalance) {
+            console.log(chalk.red("Insufficient Balance!"));
+        }
+        else {
+            myBalance -= amountAns.amount;
+            console.log(chalk.green(`Your remaining balance is: ${myBalance}`));
+        }
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log(chalk.green(`Your balance is: ${myBalance}`));
+    }
+    else if (operationAns.operation === "Fast Cash") {
+        let fastCash = await inquirer.prompt([
+            {
+                name: "amount",
+                message: chalk.cyan("Select an amount for Fast Cash "),
+                type: "list",
+                choices: [1000, 2000, 5000, 10000]
+            }
+        ]);
+        myBalance -= fastCash.amount;
+        console.log(chalk.green(`Your remaining balance is: ${myBalance}`));
+    }
+    else if (operationAns.operation === "Exit") {
+        console.log(chalk.gray("Thank you for using my ATM Machine. Goodbye!"));
+        process.exit();
+    }
 }
 else {
-    console.log(chalk.red("Please select a valid operator"));
+    console.log(chalk.red("Incorrect pin code!!"));
 }
